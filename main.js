7675aab825d18eff10ddbd8dd3f2978c54e792ed
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-01-05 12:21:38 -0500

    Incorpora la estructura del html y el JS para el funcionamiento base del modal

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+const btnNuevoLibro = document.getElementById('nuevoLibro')
+const modalLibro = document.getElementById('modalLibro')
+const btnCerrarModal = document.getElementById('cerrarModal')
+
+
+// comportamiento del modal
+btnNuevoLibro.addEventListener('click', () => {
+   modalLibro.showModal() 
+})
+
+btnCerrarModal.addEventListener('click', () => {
+    modalLibro.close()
+})
+
+// cerral modal al hacer clic por fuera del modal
+modalLibro.addEventListener("click", e => {
+    const dialogDimensions = modalLibro.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modalLibro.close()
+    }
+})
+
 console.log('patito')
