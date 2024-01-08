aee46cae1f4f6b16faaa9accb8637f6760c58721
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-01-08 10:11:53 -0500

    Crea las fichas desde JS

--- main.js
+++ main.js
@@ -17,8 +17,6 @@ function Libro(titulo, autor, extension, anno, leido, descripcion, url, img) {
     this.img = !img ? `https://picsum.photos/id/${this.id}/300/200` : `https://${img}`
 }
 
-// adecuar los titulos para la búsqueda en goolge
-//
 function crearFicha(indice){
     const ficha = document.createElement('div')
     ficha.classList.add('ficha')
@@ -57,36 +55,77 @@ function crearFicha(indice){
 
     const acciones = document.createElement('div')
     acciones.classList.add('ficha__acciones')
-    acciones.textContent = 'acciones 123 4567'
 
-    const irWeb = document.createElement('button')
-    const irWebSR = document.createElement('span')
-    irWebSR.classList.add('sr-only')
-    irWebSR.textContent = 'Ver el libro'
+    const irBTN = document.createElement('a')
+    irBTN.setAttribute('href', biblioteca[indice].url)
+    irBTN.setAttribute('target', '_blank')
+    const irSR = document.createElement('span')
+    irSR.classList.add('sr-only')
+    irSR.textContent = 'Ver el libro'
+    const irSVG = document.createElement('span')
+    irSVG.classList.add('material-symbols-outlined')
+    irSVG.setAttribute('aria-hidden', 'true')
+    irSVG.textContent = 'search'
+    irBTN.appendChild(irSR)
+    irBTN.appendChild(irSVG)
 
-    const borrar = document.createElement('button')
+    const lecturaBTN = document.createElement('button')
+    lecturaBTN.setAttribute('data-leido', biblioteca[indice].leido)
+    const lecturaSR = document.createElement('span')
+    lecturaSR.classList.add('sr-only')
+    lecturaSR.textContent = 'Marcar como libro'
+    const leerSpan = document.createElement('span')
+    leerSpan.classList.add('ficha__leer')
+    const leerSR = document.createElement('span')
+    leerSR.classList.add('sr-only')
+    leerSR.textContent = 'por leer'
+    const leerSVG = document.createElement('span')
+    leerSVG.classList.add('material-symbols-outlined')
+    leerSVG.setAttribute('aria-hidden', 'true')
+    leerSVG.textContent = 'book_5'
+    const leidoSpan = document.createElement('span')
+    leidoSpan.classList.add('ficha__leido')
+    const leidoSR = document.createElement('span')
+    leidoSR.classList.add('sr-only')
+    leidoSR.textContent = 'leído'
+    const leidoSVG = document.createElement('span')
+    leidoSVG.classList.add('material-symbols-outlined')
+    leidoSVG.setAttribute('aria-hidden', 'true')
+    leidoSVG.textContent = 'menu_book'
+    leerSpan.appendChild(leerSR)
+    leerSpan.appendChild(leerSVG)
+    leidoSpan.appendChild(leidoSR)
+    leidoSpan.appendChild(leidoSVG)
+    lecturaBTN.appendChild(lecturaSR)
+    lecturaBTN.appendChild(leerSpan)
+    lecturaBTN.appendChild(leidoSpan)
+
+    const editarBTN = document.createElement('button')
+    const editarSR = document.createElement('span')
+    editarSR.classList.add('sr-only')
+    editarSR.textContent = 'editar'
+    const editarSVG = document.createElement('span')
+    editarSVG.classList.add('material-symbols-outlined')
+    editarSVG.setAttribute('aria-hidden', 'true')
+    editarSVG.textContent = 'edit'
+    editarBTN.appendChild(editarSR)
+    editarBTN.appendChild(editarSVG)
+
+    const borrarBTN = document.createElement('button')
     const borrarSR = document.createElement('span')
     borrarSR.classList.add('sr-only')
-    borrarSR.textContent = 'Borrar'
-
-    const contenedorSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    contenedorSVG.setAttribute('width', '24')
-    contenedorSVG.setAttribute('height', '24')
-    contenedorSVG.setAttribute('viewBox', '0 0 24 24')
-    contenedorSVG.setAttribute('aria-hidden', 'true')
-
-    const irWebPath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-    irWebPath.setAttribute('fill', 'currentColor')
-    irWebPath.setAttribute('d', 'M16.36 14c.08-.66.14-1.32.14-2c0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2m-5.15 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95a8.03 8.03 0 0 1-4.33 3.56M14.34 14H9.66c-.1-.66-.16-1.32-.16-2c0-.68.06-1.35.16-2h4.68c.09.65.16 1.32.16 2c0 .68-.07 1.34-.16 2M12 19.96c-.83-1.2-1.5-2.53-1.91-3.96h3.82c-.41 1.43-1.08 2.76-1.91 3.96M8 8H5.08A7.923 7.923 0 0 1 9.4 4.44C8.8 5.55 8.35 6.75 8 8m-2.92 8H8c.35 1.25.8 2.45 1.4 3.56A8.008 8.008 0 0 1 5.08 16m-.82-2C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2c0 .68.06 1.34.14 2M12 4.03c.83 1.2 1.5 2.54 1.91 3.97h-3.82c.41-1.43 1.08-2.77 1.91-3.97M18.92 8h-2.95a15.65 15.65 0 0 0-1.38-3.56c1.84.63 3.37 1.9 4.33 3.56M12 2C6.47 2 2 6.5 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2')
-
-    const borrarPath= document.createElementNS('http://www.w3.org/2000/svg', 'path')
-    irWebPath.setAttribute('fill', 'currentColor')
-    irWebPath.setAttribute('d', 'M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z')
-
-    contenedorSVG.appendChild(irWebPath)
-    irWeb.appendChild(irWebSR)
-    irWeb.appendChild(contenedorSVG)
-    acciones.appendChild(irWeb)
+    borrarSR.textContent = 'borrar'
+    const borrarSVG = document.createElement('span')
+    borrarSVG.classList.add('material-symbols-outlined')
+    borrarSVG.setAttribute('aria-hidden', 'true')
+    borrarSVG.textContent = 'delete'
+    borrarBTN.appendChild(borrarSR)
+    borrarBTN.appendChild(borrarSVG)
+    
+    acciones.appendChild(irBTN)
+    acciones.appendChild(lecturaBTN)
+    acciones.appendChild(editarBTN)
+    acciones.appendChild(borrarBTN)
 
     ficha.appendChild(imagen)
     autoria.appendChild(informacion)
@@ -95,7 +134,6 @@ function crearFicha(indice){
     ficha.appendChild(acciones)
 
     displayLibros.appendChild(ficha)
-
 }
 
 function agregarLibro(...informacion) {
@@ -117,13 +155,12 @@ agregarLibro(
     'Milan Kundera',
     230,
     1987,
-    true,
+    false,
     'Kundera explora la vida, el amor y la insignificancia existencial en un mundo de decisiones y consecuencias ligeras.',
     '',
     'img2.rtve.es/v/4836164/?w=1600'
 )
 
-console.log(biblioteca[1].img)
 // comportamiento del modal
 btnNuevoLibro.addEventListener('click', () => {
    modalLibro.showModal() 
@@ -134,6 +171,7 @@ btnCerrarModal.addEventListener('click', () => {
 })
 
 crearFicha(0)
+crearFicha(1)
 
 // cerral modal al hacer clic por fuera del modal
 modalLibro.addEventListener("click", e => {
