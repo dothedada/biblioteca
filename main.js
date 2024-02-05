ed9287637014baa3056a3db549707ee7d274fce3
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-02-04 20:04:48 -0500

    Integra el ordenado de la biblioteca, corrige problemas de implementación

--- main.js
+++ main.js
@@ -12,24 +12,29 @@ const lib = (() => {
     const update = (reference, updatedBook) => {
         const arrIndex = shelf.findIndex(book => book.bookID === reference)
         shelf[arrIndex] = updatedBook
-        console.table(shelf)
     }
     const remove = reference => {
         const arrIndex = shelf.findIndex(book => book.bookID === reference)
         shelf.splice(arrIndex, 1)
     }
-    const arrange = (by, order) => {
+    const arrange = (order, by = undefined) => {
         if (by === 'title') shelf.sort((a, b) => {
-            return a.title.localeCompare(b.title)
+            if (order) return a.title.localeCompare(b.title)
+            return b.title.localeCompare(a.title)
         })
         if (by === 'author') shelf.sort((a, b) => {
-            return a.author.localeCompare(b.author)
+            if (order) return a.author.localeCompare(b.author)
+            return b.author.localeCompare(a.author)
         })
         if (by === 'year') shelf.sort((a, b) => {
-            return +a.year - b.year
+            if (order) return +a.year - +b.year
+            return +b.year - +a.year
         })
+        if (by === 'order') {
+            shelf.reverse()
+        }
 
-        if(order)shelf.reverse()
+        console.table(shelf)
     }
 
     return { index, add, update, remove, arrange, shelf}
@@ -75,32 +80,37 @@ class Book {
 // Habilitar modal para la creación de nuevos libros
 // habilitar la ordenada de los libros
 
-
+for (const barBTN of document.querySelectorAll('#orderLibrary > label')) {
+    barBTN.addEventListener('click', () => {
+        console.log(document.querySelector('#order').checked)
+        lib.arrange(document.querySelector('#order').checked, barBTN.getAttribute('for'))
+    })
+}
 // Habilitar el modal para la creación o carga de un nuevo libro
 
 // Lib
 // Crear las fichas a partir de la libreria
 // habilitar funcionalidad de edicion
 //
-const book1 = new Book(
-    "Matadero 5",
-    "test_K. Vonnegut",
-    "http://nidodelibros.com/wp-content/uploads/2023/02/9780440180296.jpeg",
-    "Chronicles a soldier's time-jumping journey through war, trauma, and surreal alien experiences.",
-    "255",
-    "1969",
-    "http://nidodelibros.com/producto/slaughterhouse-five/",
-    "true"
-)
-const book2 = new Book(
-    "El Artesano",
-    "test_Richard Sennett",
-    "http://www.anagrama-ed.es/uploads/media/portadas/0001/26/52fd64b30c776d826b4b640a46dfff9c971a7ab7.jpeg",
-    "El artesano de Sennett aboga por la excelencia, conexión con el trabajo y el valor del proceso en la era moderna.",
-    "360",
-    "2008",
-    "http://www.anagrama-ed.es/libro/compactos/el-artesano/9788433960917/CM_768",
-    "true"
-)
+// const book1 = new Book(
+//     "Matadero 5",
+//     "test_K. Vonnegut",
+//     "http://nidodelibros.com/wp-content/uploads/2023/02/9780440180296.jpeg",
+//     "Chronicles a soldier's time-jumping journey through war, trauma, and surreal alien experiences.",
+//     "255",
+//     "1969",
+//     "http://nidodelibros.com/producto/slaughterhouse-five/",
+//     "true"
+// )
+// const book2 = new Book(
+//     "La insoportable levedad del Ser",
+//     "Milan Kundera",
+//     "https://revistasantiago.cl/cms/wp-content/uploads/2023/07/kundera.webp",
+//     "",
+//     "360",
+//     "1989",
+//     "",
+//     "false"
+// )
 
 console.table(lib.shelf)
